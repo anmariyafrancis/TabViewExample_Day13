70383dc390440e11b2bfd1f049d73ee227efe069
anmariyafrancis <60163786+anmariyafrancis@users.noreply.github.com>
2020-03-23 18:25:35 -0400

    Update ThirdViewController.swift

--- TabViewExample/ThirdViewController.swift
+++ TabViewExample/ThirdViewController.swift
@@ -15,7 +15,7 @@ class ThirdViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        imgCircus.image = UIImage(named: "")
         tabBarItem.badgeValue = String(100)
     }
     
